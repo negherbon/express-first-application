d80444ab3b093e2b41ebf7b0bd6937a6ca26b5ab
Rafael Negherbon <rafael.negherbon@gmail.com>
2016-02-02 18:52:06 -0200

    Create form and route to save new product

--- app/infra/ProductsDAO.js
+++ app/infra/ProductsDAO.js
@@ -6,6 +6,10 @@ ProductsDAO.prototype.list = function(callback){
   this._connection.query('select * from products', callback);
 };
 
+ProductsDAO.prototype.save = function(product, callback){
+  this._connection.query('insert into products set ?', product, callback);
+};
+
 module.exports = function(){
   return ProductsDAO;
 };
--- app/routes/products.js
+++ app/routes/products.js
@@ -1,14 +1,24 @@
 module.exports = function(app) {
   app.get('/products', function(req, res){
     var connection = app.infra.connectionFactory();
-    var productsDb = new app.infra.ProductsDAO(connection);
-
-    productsDb.list(function(error, results){
+    var productsDAO = new app.infra.ProductsDAO(connection);
+    productsDAO.list(function(error, results){
       res.render('products/list', {
         products: results
       });
     });
-
     connection.end();
+  });
+
+  app.get('/products/new', function(req, res){
+    res.render('products/form');
+  });
+
+  app.post('/products/save', function(req, res){
+    var connection = app.infra.connectionFactory();
+    var productsDAO = new app.infra.ProductsDAO(connection);
+    productsDAO.save(req.body, function(error, result){
+      res.redirect('/products');
+    });
   });
 };
