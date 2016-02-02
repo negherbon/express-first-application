6be0b48a28855aeac3d043da325f490182b451f8
Rafael Negherbon <rafael.negherbon@gmail.com>
2016-02-02 19:52:00 -0200

    Validate required fields

--- app/routes/products.js
+++ app/routes/products.js
@@ -23,6 +23,15 @@ module.exports = function(app) {
 
   app.post('/products', function(req, res){
     var product = req.body;
+    req.assert('title', 'Title is required').notEmpty();
+    req.assert('price', 'Format invalid').isFloat();
+    var errors = req.validationErrors();
+    if (errors){
+      res.render('products/form', {
+        validationErrors: errors
+      });
+      return;
+    }
     var connection = app.infra.connectionFactory();
     var productsDAO = new app.infra.ProductsDAO(connection);
     productsDAO.save(product, function(error, result){
